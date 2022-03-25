8fc6515bd99ccceaf16aca8ff885b28780c9e69f
“KANG” <2983973848@qq.com>
2022-03-25 17:07:43 +0800

    '解决自定义错误冲突'

---
+++ schema-tests/test1.js
@@ -0,0 +1,41 @@
+const Ajv = require('ajv')
+const ajv = new Ajv({ allErrors: true }) // options can be passed, e.g. {allErrors: true}
+require('ajv-errors')(ajv /*, {singleError: true} */)
+// const localize = require('ajv-i18n')
+
+const schema = {
+    type: 'object',
+    properties: {
+        foo: { type: 'integer' },
+        bar: {
+            type: 'string',
+            minLength: 10,
+            errorMessage: {
+                type: '必须是字符串23333',
+                minLength: '替换原有错误信息5555555',
+            },
+        },
+    },
+    additionalProperties: false,
+    // errorMessage: {
+    //     properties: {
+
+    //     }
+    // }
+}
+
+const validate = ajv.compile(schema)
+
+const data = {
+    foo: 12,
+    bar: 3333,
+}
+
+const valid = validate(data)
+
+if (!valid) {
+    console.log(validate.errors)
+    // localize.zh(validate.errors)
+
+    // console.log(ajv.errorsText(validate.errors, { separator: '\n' }))
+}
