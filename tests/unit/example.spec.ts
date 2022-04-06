b080c3f9a6248598dfe56c7c81e1243e677c9d0f
“KANG” <2983973848@qq.com>
2022-04-06 16:25:35 +0800

    尝试jest单元测试

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,48 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount, shallowMount } from '@vue/test-utils'
+import { defineComponent, h } from 'vue'
+
+import JsonSchemaForm, { NumberField } from '../../lib'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+  it('should render correct number field', () => {
+    // 创建测试流程
+    let value = ''
+    const wrapper = mount(JsonSchemaForm, {
+      props: {
+        schema: {
+          type: 'number',
+        },
+        value: value,
+        onChange: (v: any) => {
+          value = v
+        },
+      },
     })
-    expect(wrapper.text()).toMatch(msg)
+
+    // 渲染 numberField
+    const numberField = wrapper.findComponent(NumberField)
+    // expect(numberField.exists()).toBeTruthy()
   })
 })
+
+// const HelloWorld = defineComponent({
+//   name: 'HelloWorld',
+//   props: {
+//     msg: String,
+//   },
+//   setup(props) {
+//     return () => {
+//       return h('div', props.msg)
+//     }
+//   },
+// })
+
+// describe('HelloWorld.vue', () => {
+//   it('renders props.msg when passed', () => {
+//     const msg = 'new message'
+//     const wrapper = shallowMount(HelloWorld, {
+//       props: { msg },
+//     })
+//     expect(wrapper.text()).toMatch(msg)
+//   })
+// })
